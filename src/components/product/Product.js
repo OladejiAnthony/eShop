a493bf8c6cb6f3d560598f1ffe1a4ac14a9a3d9c
oladeji anthony <oladejianthony98@gmail.com>
2024-03-05 18:38:20 +0100

    homepage filter functionalities completed

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Product.module.scss";
 import ProductFilter from "./productFilter/ProductFilter";
 import ProductList from "./productList/ProductList";
 import useFetchCollection from "../../customHooks/useFetchCollection";
 import { useDispatch, useSelector } from "react-redux";
-import { STORE_PRODUCTS, selectProducts } from "../../redux/slice/productSlice";
+import {
+  GET_PRICE_RANGE,
+  STORE_PRODUCTS,
+  selectProducts,
+} from "../../redux/slice/productSlice";
 import spinnerImg from "../../assets/spinner.jpg";
+import { FaCogs } from "react-icons/fa";
 
 const Product = () => {
   const { data, isLoading } = useFetchCollection("products"); //The collectionName prop value is "products"
+  const [showFilter, setShowFilter] = useState(false);
 
   const dispatch = useDispatch();
   const products = useSelector(selectProducts);
@@ -21,13 +27,26 @@ const Product = () => {
       })
     );
     //console.log(data)
+    dispatch(
+      GET_PRICE_RANGE({
+        products: data,
+      })
+    );
   }, [dispatch, data]); //dispatch products when data changes
+
+  const toggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
 
   return (
     <section>
       <div className={`container ${styles.product}`}>
-        <aside className={styles.filter}>
-          {isLoading ? null : <ProductFilter /> }
+        <aside
+          className={
+            showFilter ? `${styles.filter} ${styles.show}` : `${styles.filter}`
+          }
+        >
+          {isLoading ? null : <ProductFilter />}
         </aside>
 
         <div className={styles.content}>
@@ -41,6 +60,13 @@ const Product = () => {
           ) : (
             <ProductList products={products} />
           )}
+          {/*Mobile View */}
+          <div className={styles.icon} onClick={toggleFilter}>
+            <FaCogs size={20} color="orangered" />
+            <p>
+              <b>{showFilter ? "Hide Filter" : "Show Filter"}</b>
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/components/product/productFilter/ProductFilter.js
+++ src/components/product/productFilter/ProductFilter.js
@@ -1,78 +1,123 @@
-import React, { useEffect, useState } from 'react'
-import styles from "./ProductFilter.module.scss"
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import styles from "./ProductFilter.module.scss";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  FILTER_BY_CATEGORY} from "../../../redux/slice/filterSlice"
-import { selectProducts } from '../../../redux/slice/productSlice';
-
-  
+  FILTER_BY_CATEGORY,
+  FILTER_BY_BRAND,
+  FILTER_BY_PRICE,
+} from "../../../redux/slice/filterSlice";
+import {
+  selectMaxPrice,
+  selectMinPrice,
+  selectProducts,
+} from "../../../redux/slice/productSlice";
 
 const ProductFilter = () => {
-  
   const products = useSelector(selectProducts);
+  const minPrice = useSelector(selectMinPrice);
+  const maxPrice = useSelector(selectMaxPrice);
+  const dispatch = useDispatch();
+
   const allCategories = [
     "All", //added ALL to the xisting categories
-    ...new Set(products.map((product) => product.category)) 
-  ]
-  //console.log(allCategories)
-  const [category, setCategory] = useState("ALL");
+    ...new Set(products.map((product) => product.category)),
+  ]; //console.log(allCategories)
+  const allBrands = [
+    "All", //added ALL to the xisting categories
+    ...new Set(products.map((product) => product.brand)),
+  ]; //console.log(allBrands)
 
-  const dispatch = useDispatch();
-  const filterProducts = (cat) => {
-    setCategory(cat);
-    dispatch(FILTER_BY_CATEGORY({
-      products,
-      category: cat
-  }))
-  }
-
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [price, setPrice] = useState(4000);
 
   //Category Filter logic
-  // useEffect(() => {
-  //   //console.log(sort);
-  //   dispatch(
-  //     FILTER_BY_CATEGORY({
-  //       products,
-  //       category,
-  //     })
-  //   );
-  // }, [category, products, dispatch]);
+  const filterProducts = (cat) => {
+    setCategory(cat);
+    dispatch(
+      FILTER_BY_CATEGORY({
+        products,
+        category: cat,
+      })
+    );
+  };
 
+  //Brand Filter logic
+  useEffect(() => {
+    dispatch(
+      FILTER_BY_BRAND({
+        products,
+        brand,
+      })
+    );
+  }, [products, brand, dispatch]);
+
+  const clearFilters = () => {
+    setCategory("All");
+    setBrand("All");
+    setPrice(maxPrice);
+  }
+
+  //Price Filter logic
+  useEffect(() => {
+    dispatch(
+      FILTER_BY_PRICE({
+        products,
+        price,
+      })
+    );
+  }, [products, price, dispatch]);
 
   return (
     <div className={styles.filter}>
       <h4>Categories</h4>
       <div className={styles.category}>
-        {
-          allCategories.map((cat, index) => {
-            return (
-              <button key={index} type='button' className={`${category}` === cat ? `${styles.active}` : null } onClick={() => filterProducts(cat)}>
-                &#8250; {cat}
-              </button>
-            )
-          })
-        }
+        {allCategories.map((cat, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={`${category}` === cat ? `${styles.active}` : null}
+              onClick={() => filterProducts(cat)}
+            >
+              &#8250; {cat}
+            </button>
+          );
+        })}
       </div>
 
       <h4>Brand</h4>
       <div className={styles.brand}>
-        <select name='brand'>
-          <option value="all">All</option>
+        <select
+          name="brand"
+          value={brand}
+          onChange={(e) => setBrand(e.target.value)}
+        >
+          {allBrands.map((brand, index) => {
+            return (
+              <option key={index} value={brand}>
+                {brand}
+              </option>
+            );
+          })}
         </select>
       </div>
 
       <h4>Price</h4>
-      <p>1500</p>
+      <p>{`$ ${price}`}</p>
       <div className={styles.price}>
-        <input type='range' name='price' min="100" max="1000"   />
+        <input
+          type="range"
+          min={minPrice}
+          max={maxPrice}
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
       </div>
       <br />
-      <button className='--btn --btn-danger'>Clear Filter</button>
-
+      <button className="--btn --btn-danger" onClick={clearFilters}>Clear Filter</button>
     </div>
-  )
-}
+  );
+};
 
-export default ProductFilter
-
-
+export default ProductFilter;
